7e9ed929694c4a6a816f81acc1cc247197b02c05
Chamal Weerasinghe <chamalwr@users.noreply.github.com>
2022-03-18 01:15:37 +0530

    added home module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AssestsManagerCommonModule } from './assests-manager-common/assests-manager-common.module';
+import { HomeModule } from './home/home.module';
 import { IncomeCategoryModule } from './income-category/income-category.module';
 import { IncomeSheetModule } from './income-sheet/income-sheet.module';
 
@@ -14,9 +15,10 @@ import { IncomeSheetModule } from './income-sheet/income-sheet.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HomeModule,
+    AssestsManagerCommonModule,
     IncomeSheetModule,
     IncomeCategoryModule,
-    AssestsManagerCommonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
