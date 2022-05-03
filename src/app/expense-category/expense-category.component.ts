675fceea98ababdd180db02870fa594b6f84ee91
Chamal Weerasinghe <chamalwr@users.noreply.github.com>
2022-05-03 19:57:24 +0530

    added expense category module template

---
+++ src/app/expense-category/expense-category.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'amgr-expense-category',
+  templateUrl: './expense-category.component.html',
+  styleUrls: ['./expense-category.component.scss']
+})
+export class ExpenseCategoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/expense-category/expense-category.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ExpenseCategoryRoutingModule } from './expense-category-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ExpenseCategoryRoutingModule
+  ]
+})
+export class ExpenseCategoryModule { }
