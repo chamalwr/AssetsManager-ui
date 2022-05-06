d6831afe0bcde3ff82a1b98d06cde772db432d7b
Chamal Weerasinghe <chamalwr@users.noreply.github.com>
2022-05-06 22:44:56 +0530

    added expense sheet queries

--- src/app/assests-manager-common/service/expense-sheet.service.ts
+++ src/app/assests-manager-common/service/expense-sheet.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Apollo } from 'apollo-angular';
-import { GET_EXPENSE_SHEET, GET_EXPENSE_SHEETS, GET_EXPENSE_SHEETS_SUMMARY } from '../query/expense-sheet.query';
+import { CREATE_EXPENSE_SHEET, DELETE_EXPENSE_SHEET, GET_EXPENSE_SHEET, GET_EXPENSE_SHEETS, GET_EXPENSE_SHEETS_BY_YEAR, GET_EXPENSE_SHEETS_SUMMARY, GET_EXPENSE_SHEET_BY_MONTH_AND_YEAR, UPDATE_EXPENSE_SHEET } from '../query/expense-sheet.query';
 
 @Injectable({
   providedIn: 'root'
@@ -40,5 +40,58 @@ export class ExpenseSheetService {
     });
 
     return expenseSheet.valueChanges;
-  }  
+  }
+  
+  getExpenseSheetsByMonthAndYear(userId: string, month: number, year: number){
+    const expenseSheets = this.apolloClient.watchQuery({
+      query: GET_EXPENSE_SHEET_BY_MONTH_AND_YEAR,
+      variables: {
+        userId: userId,
+        month: month,
+        year: year,
+      }
+    });
+
+    return expenseSheets.valueChanges;
+  }
+
+  getExpenseSheetsByYear(userId: string, year: number){
+    const expenseSheets = this.apolloClient.watchQuery({
+      query: GET_EXPENSE_SHEETS_BY_YEAR,
+      variables: {
+        userId: userId,
+        year: year
+      }
+    });
+
+    return expenseSheets.valueChanges;
+  }
+
+  createExpenseSheet(createExpenseSheetDto: any){
+    return this.apolloClient.mutate({
+      mutation: CREATE_EXPENSE_SHEET,
+      variables: {
+        createExpenseSheetInput:  createExpenseSheetDto
+      }
+    });
+  }
+
+  deleteExpenseSheet(id: string){
+    return this.apolloClient.mutate({
+      mutation: DELETE_EXPENSE_SHEET,
+      variables: {
+        id: id
+      }
+    });
+  }
+
+  updateExpenseSheet(id: string, updateExpenseSheetDto: any){
+    return this.apolloClient.mutate({
+      mutation: UPDATE_EXPENSE_SHEET,
+      variables: {
+        id: id,
+        updateExpenseSheetInput: updateExpenseSheetDto
+      }
+    });
+  }
 }
