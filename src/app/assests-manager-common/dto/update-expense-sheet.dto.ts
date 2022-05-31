1658a730e2d0eff287b10d38f94342095fb925df
Chamal Weerasinghe <chamalwr@users.noreply.github.com>
2022-06-01 01:39:17 +0530

    added expense sheet update

---
+++ src/app/assests-manager-common/dto/update-expense-sheet.dto.ts
@@ -0,0 +1,5 @@
+export class UpdateExpenseSheetDto {
+    year!: number;
+    month!: number;
+    currency!: string;
+}
--- src/app/expense-sheet/components/expense-sheet-all-view/expense-sheet-all-view.component.ts
+++ src/app/expense-sheet/components/expense-sheet-all-view/expense-sheet-all-view.component.ts
@@ -1,8 +1,9 @@
 import { DecimalPipe } from '@angular/common';
 import { Component, EventEmitter, OnInit, Output, PipeTransform, Type } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ModalDismissReasons, NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ToastrService } from 'ngx-toastr';
+import { UpdateExpenseSheetDto } from 'src/app/assests-manager-common/dto/update-expense-sheet.dto';
 import { ExpenseSheetSummary } from 'src/app/assests-manager-common/entity/expense-sheet-summary.entity';
 import { ExpenseSheetService } from 'src/app/assests-manager-common/service/expense-sheet.service';
 import { environment } from 'src/environments/environment';
@@ -19,6 +20,11 @@ export class ExpenseSheetAllViewComponent implements OnInit {
   filter = new FormControl('');
   loading: boolean = false;
   closeResult: string = "";
+
+  editExpenseSheetForm = new FormGroup({
+    date: new FormControl(null, [Validators.required]),
+    currency: new FormControl(null, [Validators.required]),
+  });
 
   constructor(
     private expenseSheetService: ExpenseSheetService,
@@ -71,11 +77,49 @@ export class ExpenseSheetAllViewComponent implements OnInit {
     });
   }
 
-  private getDismissReason(reason: any): void {
-    if (reason === ModalDismissReasons.ESC) {
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-    } else {
-    }
+  editExpenseSheet(content: any, expenseSheet: ExpenseSheetSummary){
+    this.editExpenseSheetForm.setValue({
+      date: { year: expenseSheet.year, month: expenseSheet.month, day: 1 },
+      currency: expenseSheet.currency,
+    });
+    this.modalService.open(content, { animation: true, centered: true, ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      if(result.status === 'VALID'){
+        const updateExpenseSheetDto: UpdateExpenseSheetDto = {
+          year: result.value.date.year,
+          month: result.value.date.month,
+          currency: result.value.currency
+        }
+        this.confirmUpdateExpenseSheet(expenseSheet._id, updateExpenseSheetDto);
+      }
+    }, (reason) => {
+    });
+  }
+
+  private confirmUpdateExpenseSheet(expenseSheetId: string, updateExpenseSheetDto: UpdateExpenseSheetDto){
+    const data = this.expenseSheetService.updateExpenseSheet(expenseSheetId, updateExpenseSheetDto);
+    data.subscribe({
+      next: (result: any) => {
+        if(result.loading){
+          this.loading = true
+        }else {
+          if(result.data.updateExpenseSheet && result.data.updateExpenseSheet['__typename'] === 'ExpenseSheet'){
+            this.loading = false;
+            this.toastr.success('Expense Sheet Details Updated Complete', 'Expense Sheet Update');
+          }else if(result.data.updateExpenseSheet && result.data.updateExpenseSheet['__typename'] === 'ExpenseSheetResultError') {
+            const errorModel = result.data.updateExpenseSheet;
+            this.loading = false;
+            this.toastr.warning(errorModel.reason, errorModel.message);
+          }else {
+            this.loading = false;
+            this.toastr.error('Something went wrong, Update Failed', 'Expense Sheet Update Failed');
+          }
+        }
+      },
+      error: (e) => {
+        this.loading = false;
+        this.toastr.error('Something went wrong, Update Failed', 'Expense Sheet Update Failed');
+      }
+    });
   }
 
   private deleteExpenseSheet(id: string){
@@ -107,6 +151,13 @@ export class ExpenseSheetAllViewComponent implements OnInit {
   }
 
   search(text: string, pipe: PipeTransform) {
+  }
+
+  private getDismissReason(reason: any): void {
+    if (reason === ModalDismissReasons.ESC) {
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+    } else {
+    }
   }
 
 }
